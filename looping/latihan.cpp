9de57c2f8fb74dc2ff1cd5d6e25cab5c493557a9
Syamil Azzimul <syamilazzimulbasrhy@gmail.com>
2024-09-22 09:26:15 +0700

    function case 1

--- looping/latihan.cpp
+++ looping/latihan.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 using namespace std;
+
+const int MAX_MUSIK = 10;
+string tambahMusik(string judulLagu, string listMusik[], int &index) {
+    if(index >= MAX_MUSIK){
+        return "List musik sudah penuh!";
+    } 
+    else{
+        listMusik[index] = judulLagu;
+        index++;
+        return "Musik berhasil ditambahkan!";
+    }
+}
+
 int main (){
-    int x;  
-    string musik[10]= {};
+    int x;
+    string judulLagu;
+    string pesan;
+    string listMusik[10];
+    int index = 0;
+
     do {
         cout << "===== Welcome to Program Musik =====" << endl;
         cout << "1. Tambah Musik" << endl;
@@ -12,11 +29,18 @@ int main (){
         cin >> x;
         switch (x){
             case 1:
+                cout << "Masukkan judul lagu: ";
+                cin>>judulLagu;
+                pesan = tambahMusik(judulLagu, listMusik, index);
+                cout << pesan << endl;
             break;
             case 2:
+            break;
             case 3:
+            break;
             case 4:
-                cout << "Sistem telah berhenti";
+            cout << "Sistem telah berhenti";
+            break;
             default :
                 cout << "Input harus 1-4"<< endl;
         }
